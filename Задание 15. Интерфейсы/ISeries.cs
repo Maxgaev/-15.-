cbfa37ff58db4e21e32bf31ab15c2504368a513f
Максим Алехин <Max-crf@mail.ru>
2022-01-30 20:42:06 +0700

    Добавьте файлы проекта.

---
+++ Задание 15. Интерфейсы/ISeries.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_15.Интерфейсы
+{
+    interface ISeries
+    {
+        void setStart(int firstvalue);
+        int getNext();
+        void reset();
+    }
+
+    class ArithProgression : ISeries
+    {
+        int StepValue;
+        int FirstValue;
+        int CurrentValue;
+        public ArithProgression(int firstValue, int stepValue)
+        {
+            FirstValue = firstValue;
+            CurrentValue = firstValue;
+            StepValue = stepValue;
+        }
+        public int getNext()
+        {
+            CurrentValue += StepValue;
+            return CurrentValue;
+        }
+
+        public void reset()
+        {
+            CurrentValue = FirstValue;
+        }
+
+        public void setStart(int firstvalue)
+        {
+            FirstValue = firstvalue;
+            CurrentValue = FirstValue;
+        }
+        public void setStep(int step)
+        {
+            StepValue = step;
+        }
+    }
+
+    class GeomProgression : ISeries
+    {
+        int FirstValue;
+        int CurrentValue;
+        int DenomValue;
+        public GeomProgression(int firstValue, int denomValue)
+        {
+            FirstValue = firstValue;
+            CurrentValue = firstValue;
+            DenomValue = denomValue;
+        }
+        public int getNext()
+        {
+            CurrentValue *= DenomValue;
+            return CurrentValue;
+        }
+        public void reset()
+        {
+            CurrentValue = FirstValue;
+
+        }
+        public void setStart(int firstValue)
+        {
+            FirstValue = firstValue;
+            CurrentValue = FirstValue;
+        }
+    }
+
+
+    
+}
---
+++ Задание 15. Интерфейсы/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_15.Интерфейсы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        calc:
+            Console.WriteLine("Введите начальное значение прогрессии целое число:");
+            int firstValue = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("\nВведите шаг арифметической прогрессии:");
+            int stepValue = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("\nВведите шаг геометрической прогрессии:");
+            int denomValue = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("\nВведите количество членов прогрессий:");
+            int number = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            ArithProgression arith = new ArithProgression(firstValue, stepValue);
+            Console.WriteLine("\nАрифметическая прогрессия:\n");
+            Console.Write($"\t{firstValue} ");
+            for (int i = 0; i < number - 1; i++)
+            {
+                Console.Write($"\t{arith.getNext()} ");
+            }
+            Console.WriteLine();
+
+            GeomProgression geom = new GeomProgression(firstValue, denomValue);
+            Console.WriteLine("\nГеометрическая прогрессия:\n");
+            Console.Write($"\t{firstValue} ");
+            for (int i = 0; i < number - 1; i++)
+            {
+                Console.Write($"\t{geom.getNext()} ");
+            }
+
+            Console.WriteLine("\nесли хотите начать заново  - Нажмите <Enter>, \nДля выхода, нажмите любую клавишу...");
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+                Console.Clear();
+                goto calc;
+
+            }
+            Console.ReadKey();
+
+            arith.setStart(firstValue);
+            arith.setStep(stepValue);
+
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            arith.reset();
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            geom.reset();
+
+
+        }
+
+    }
+}
